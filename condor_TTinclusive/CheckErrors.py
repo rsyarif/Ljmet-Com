89b5357722bae3dde755a90dbe79a450cf0e4124
jmhogan <jmhogan@fnal.gov>
2019-02-05 11:21:04 -0600

    rearrange CheckErrors, small updates to submitters

--- condor_TTinclusive/CheckErrors.py
+++ condor_TTinclusive/CheckErrors.py
@@ -1,5 +1,5 @@
 import os, sys, getopt
-execfile("/uscms_data/d3/varun/EOSSafeUtils.py")
+execfile("/uscms_data/d3/jmanagan/EOSSafeUtils.py")
 
 ## CheckErrors.py /uscms_data/d3/path/to/logs/nominal/ --verbose 1 (prints) --resubmit 1 (if you want to resubmit) --resub_num -1 (resubmits all the fails it looks for)
 
@@ -24,8 +24,7 @@ for o, a in opts:
 	if o == '--resubmit': resubmit = a
 	if o == '--resub_num': resub_num = int(a)
 
-#rootdir = '/eos/uscms/store/user/lpcljm/2016/'+dir.split('/')[-3]+'/'+dir.split('/')[-2]+'/'
-rootdir = '/eos/uscms/store/user/lpctlbsm/jmanagan/'+dir.split('/')[-3]+'/'+dir.split('/')[-2]+'/'
+rootdir = '/eos/uscms/store/user/lpcljm/2018/'+dir.split('/')[-3]+'/'+dir.split('/')[-2]+'/'
 rootdir = rootdir.replace('_logs','')
 print 'checking ROOT files in:',rootdir
 folders = [x for x in os.walk(dir).next()[1]]
@@ -41,13 +40,10 @@ empty_log = 0
 copy_fail = 0
 mem_fail = 0
 kill_fail = 0
+fatal_fail = 0
 
 for folder in folders:
-        #if folder != 'QCD_HT700to1000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_25ns': continue
-        #if 'Charged' not in folder and 'Single' not in folder: continue
-        #if 'WW' not in folder: continue
-        #if 'WJetsToLNu_Tune' not in folder: continue
-        #if 'SingleElectron' not in folder and 'Hv3' not in folder: continue
+        #if 'Bprime' not in folder: continue
 	if verbose_level > 0:  print; print folder
 
         rootfiles = EOSlist_root_files(rootdir+folder)
@@ -63,79 +59,114 @@ for folder in folders:
 		total_total+=1
 		index = file[file.find('_')+1:file.find('.')]
 		if '_' in index: index = index.split('_')[-1]
-                #if int(index) > 10: continue
 		count_total += 1
 	
+                ## CHECK THE CONDOR LOG FIRST
+                ## Memory fails, walltime fails, removals/kills, check for termination
+                term = False                
 		try:
-			current = open(dir + '/'+folder+'/'+file.replace('.jdl','.log'),'r')
-			good = False
-			for line in current:
-				if 'All cuts' in line: good = True
-			if not good: 
-				if verbose_level > 0: 
-					print '\tEMPTY LOG:',file,' and JobIndex:',index
-				empty_log+=1
-				if resub_num == -1 or resub_num == 0:resub_index.append(index)
-				continue
-		except:
-			pass
+                    current = open(dir + '/'+folder+'/'+file.replace('.jdl','.condor'),'r')
+                    overmem = False
+                    killed = False
+                    term = False
+                    iline = 0
+                    overmem_index = 0
+                    kill_index = 0
+                    term_index = 0
+                    for line in current:
+                        if 'SYSTEM_PERIODIC_REMOVE' in line: 
+                            overmem = True
+                            term = True
+                            overmem_index = iline
+                        elif 'condor_rm' in line: 
+                            killed = True
+                            term = True
+                            kill_index = iline
+                        elif 'termination' in line: 
+                            term = True
+                            term_index = iline
+                        iline += 1
 
-		try:
-			current = open(dir + '/'+folder+'/'+file.replace('.jdl','.out'),'r')
-			good = True
-			for line in current:
-				if 'failure' in line: good = False
-			if not good: 
-				if verbose_level > 0: 
-					print '\tXRDCP FAIL:',file,' and JobIndex:',index
-				copy_fail+=1
-				if resub_num == -1 or resub_num == 2:resub_index.append(index)
-				continue
-		except:
-			pass
+                    if overmem and overmem_index > term_index: 
+                        if verbose_level > 0: 
+                            print '\tMEM/TIME FAIL:',file,' and JobIndex:',index
+                        mem_fail+=1
+                        if resub_num == -1 or resub_num == 3:resub_index.append(index)
+                        continue
 
-		try:
-			current = open(dir + '/'+folder+'/'+file.replace('.jdl','.condor'),'r')
-			overmem = False
-                        killed = False
-                        term = False
-                        iline = 0
-                        overmem_index = 0
-                        kill_index = 0
-                        term_index = 0
-			for line in current:
-				if 'SYSTEM_PERIODIC_REMOVE' in line: 
-                                    overmem = True
-                                    overmem_index = iline
-                                elif 'condor_rm' in line: 
-                                    killed = True
-                                    kill_index = iline
-                                elif 'Normal termination (return value 0)' in line: 
-                                    term = True
-                                    term_index = iline
-                                iline += 1
-			if overmem and overmem_index > term_index: 
-				if verbose_level > 0: 
-					print '\tMEM FAIL:',file,' and JobIndex:',index
-				mem_fail+=1
-				if resub_num == -1 or resub_num == 3:resub_index.append(index)
-				continue
-			if killed and kill_index > term_index: 
-				if verbose_level > 0: 
-					print '\tKILL_FAIL:',file,' and JobIndex:',index
-				kill_fail+=1
-				if resub_num == -1 or resub_num == 4:resub_index.append(index)
-				continue
-		except:
-			pass
+                    if killed and kill_index > term_index: 
+                        if verbose_level > 0: 
+                            print '\tKILL_FAIL:',file,' and JobIndex:',index
+                        kill_fail+=1
+                        if resub_num == -1 or resub_num == 4:resub_index.append(index)
+                        continue
+
+                    if not term: 
+                        total_running += 1
+                            
+		except: pass
 		
+                ### JOBS STILL RUNNING, not terminated (or there was no condor log...)
+                if not term: 
+                    if os.path.exists(dir + '/'+folder+'/'+file.replace('.jdl','.condor')):
+                        if verbose_level > 1:
+                            print '\tRUNNING:',file,' and JobIndex:',index
+                        continue
+                    else:
+                        if verbose_level > 0:
+                            print '\tSUBMIT FAIL:',file,' and JobIndex:',index,' FIX MANUALLY!!'
+                        continue
+
+                ### NOW LOOK FOR FILE READ ERRORS
+                try:
+                    current = open(dir + '/'+folder+'/'+file.replace('.jdl','.err'),'r')
+                    good = True
+                    for line in current:
+                        if 'Begin Fatal Exception' in line: good = False
+                    if not good: 
+                        if verbose_level > 0: 
+                            print '\tFATAL EX:',file,' and JobIndex:',index
+                        fatal_fail+=1
+                        if resub_num == -1 or resub_num == 5:resub_index.append(index)
+                        continue
+		except: pass
+
+                ### NOW LOOK FOR COPY FAILURES
 		try:
-			if not os.path.isfile(dir+'/'+folder+'/'+file.replace('.jdl','.log')): 
-				if verbose_level > 0: 
-					print '\tNO LOG:',file,' and JobIndex:',index
-				no_log += 1
-				if resub_num == -1 or resub_num == 1: resub_index.append(index)
-				continue
+                    current = open(dir + '/'+folder+'/'+file.replace('.jdl','.out'),'r')
+                    good = True
+                    for line in current:
+                        if 'failure' in line: good = False
+                    if not good: 
+                        if verbose_level > 0: 
+                            print '\tXRDCP FAIL:',file,' and JobIndex:',index
+                        copy_fail+=1
+                        if resub_num == -1 or resub_num == 2:resub_index.append(index)
+                        continue
+		except: pass
+
+                ### UNNAMED LJMET CRASHES, LOG EXISTS BUT IS EMPTY
+		try:
+                    current = open(dir + '/'+folder+'/'+file.replace('.jdl','.log'),'r')
+                    good = False
+                    for line in current:
+                        if 'All cuts' in line: good = True
+                    if not good: 
+                        if verbose_level > 0: 
+                            print '\tEMPTY LOG:',file,' and JobIndex:',index
+                        empty_log+=1
+                        if resub_num == -1 or resub_num == 0:resub_index.append(index)
+                        continue
+		except: pass
+
+                ### ALL OTHER CRASHES, NO LJMET LOG FILE
+		try:
+                    if not os.path.isfile(dir+'/'+folder+'/'+file.replace('.jdl','.log')): 
+                        if verbose_level > 0: 
+                            print '\tNO LOG:',file,' and JobIndex:',index
+                        no_log += 1
+                        if resub_num == -1 or resub_num == 1: resub_index.append(index)
+                        continue
 		except: pass
 
 	if resub_index != []: print 'RESUBS:', resub_index
@@ -153,6 +184,8 @@ for folder in folders:
 			if line.startswith('relBase    = str('): fout.write('relBase    = str(\'CONDOR_RELBASE\')\n')
 			else: fout.write(line)
 		os.system('rm ' + dir + '/' + folder + '/' + folder.replace('/logfiles/','') + '_' + index + '.log')
+		os.system('rm ' + dir + '/' + folder + '/' + folder.replace('/logfiles/','') + '_' + index + '.err')
+		os.system('rm ' + dir + '/' + folder + '/' + folder.replace('/logfiles/','') + '_' + index + '.out')
 		os.system('rm ' + dir + '/' + folder + '/' + folder.replace('/logfiles/','') + '_' + index + '.condor')
 		os.system('condor_submit ' + dir + '/' + folder + '/' + folder.replace('/logfiles/','') + '_' + index + '.jdl')
 		indexind+=1
@@ -161,10 +194,14 @@ for folder in folders:
 
 print
 print 'TOTAL JOBS: ', total_total
+print 'ROOT files:', total_roots
+print 'TOTAL Running:', total_running
+print
 print 'NO LOG:', no_log
+print 'FATAL EX:', fatal_fail
 print 'EMPTY LOG:', empty_log
 print 'XRDCP FAIL:', copy_fail
-print 'MEMORY FAIL:', mem_fail
-print 'KILL_FAIL:', kill_fail
-print 'ROOT files:', total_roots
-print 'DONE:', total_total - no_log - empty_log - copy_fail - mem_fail - kill_fail
+print 'MEM/TIME FAIL:', mem_fail
+print 'KILLED:', kill_fail
+
+print 'DONE:', total_total - total_running - no_log - fatal_fail - empty_log - copy_fail - mem_fail - kill_fail
